d6dccf736bc446d902c0a4aafaf9ee2fc0b67cb3
WinG4merBR <ddgomes2334@outlook.com>
2022-07-02 11:01:37 -0300

    Fix audio not playing and new audios

--- app/src/main/java/com/wing4merbr/sobs/MainActivity2.kt
+++ app/src/main/java/com/wing4merbr/sobs/MainActivity2.kt
@@ -1,17 +1,34 @@
 package com.wing4merbr.sobs
 
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
 
-class MainActivity2() : AppCompatActivity() {
+
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
 
-    public fun oi() {
+     fun playOi(view: View) {
         val mp = MediaPlayer.create(this, R.raw.pui_noreverb)
-        mp.start()
+        mp.start();
+    }
+
+    fun bigbob(view: View) {
+        val mp = MediaPlayer.create(this, R.raw.molhobigbob)
+        mp.start();
+    }
+
+    fun mofo(view: View) {
+        val mp = MediaPlayer.create(this, R.raw.boobs);
+        mp.start();
+    }
+
+    fun recarga(view: View) {
+        val mp = MediaPlayer.create(this, R.raw.oirecarga);
+        mp.start();
     }
 }
